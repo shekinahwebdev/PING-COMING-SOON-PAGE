559bc998c2073a4e30915b122e3e13bfa0f72e38
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-06-10 03:01:43 +0000

    Implement logic for email validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { useFormik } from "formik";
 import "./App.css";
 const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validate: (values) => {
+      const errors = {};
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Please provide a valid email address";
+      }
+      return errors;
+    },
+  });
+
   return (
     <main className="main_page">
       <img src="/images/logo.svg" alt="Logo" className="logo" />
@@ -9,13 +27,22 @@ const App = () => {
       </p>
       <p className="subscribe_text">Subscribe and get notified</p>
 
-      <form className="form">
+      <form className="form" onSubmit={formik.hanldeSubmit}>
         <input
           type="email"
+          name="email"
           placeholder="Enter your email"
-          className="input_email"
+          onChange={formik.handleChange}
+          // className="input_email"
+          className={`input_email ${
+            formik.touched.email && formik.errors.email ? "error" : ""
+          }`}
           required
         />
+        <p className="error_message">{formik.errors.email}</p>
+        {/* <p className="error_message">
+          {formik.touched.email && formik.errors.email}
+        </p> */}
         <button type="submit" aria-label="Notify me" className="btn">
           Notify Me
         </button>
